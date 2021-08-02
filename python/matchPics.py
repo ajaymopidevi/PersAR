5f1c4ec786b40cd14ffa7b23c430cc705fe8fbeb
Ajay Narsimha M <ajaymopidevi@gmail.com>
2021-08-03 00:25:20 +0530

    Update maatchPics.py
    Test script to evaluate your detector, descriptor and matching algorithm

--- python/matchPics.py
+++ python/matchPics.py
@@ -1,24 +1,40 @@
 import numpy as np
 import cv2
 import skimage.color
-from helper import briefMatch
-from helper import computeBrief
-from helper import corner_detection
+from findCorrespondences import briefMatch
+from findCorrespondences import computeBrief
+from findCorrespondences import corner_detection
+from findCorrespondences import plotMatches
+
 
 def matchPics(I1, I2):
-	#I1, I2 : Images to match
-	
+    # I1, I2 : Images to match
 
-	#Convert Images to GrayScale
-	
-	
-	#Detect Features in Both Images
-	
-	
-	#Obtain descriptors for the computed feature locations
-	
+    # Convert Images to GrayScale
+    I1_gray = cv2.cvtColor(I1, cv2.COLOR_BGR2GRAY)
+    I2_gray = cv2.cvtColor(I2, cv2.COLOR_BGR2GRAY)
 
-	#Match features using the descriptors
-	
+    # Detect Features in Both Images
+    I1_kp = corner_detection(I1_gray)
+    I2_kp = corner_detection(I2_gray)
 
-	return matches, locs1, locs2
+    # Obtain descriptors for the computed feature locations
+    I1_des, locs1 = computeBrief(I1_gray, I1_kp)
+    I2_des, locs2 = computeBrief(I2_gray, I2_kp)
+
+    # Match features using the descriptors
+    matches = briefMatch(I1_des, I2_des)
+
+    return matches, locs1, locs2
+
+
+if __name__ == "__main__":
+    # match ratio=0.8 had 3 outliers. The outliers can be removed by redcing the ratio to 0.7
+    # However 0.7  has only 18 matches comapred to 61 matches(58 inliers) found when ratio is 0.8.
+
+    I1 = cv2.imread(r'..\data\cv_cover.jpg')
+    I2 = cv2.imread(r'..\data\cv_desk.png')
+
+    matches, locs1, locs2 = matchPics(I1, I2)
+    plotMatches(I1, I2, matches, locs1, locs2)
+
