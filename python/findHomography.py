5add1a923222db863ab8ee7ba501b646959cfdf3
Ajay Narsimha M <ajaymopidevi@gmail.com>
2021-08-06 00:50:41 +0530

    Update RANSAC code

--- python/findHomography.py
+++ python/findHomography.py
@@ -4,6 +4,7 @@ import cv2
 
 def computeH(x1, x2):
 	#Compute the homography between two sets of points
+
 	nPts = x1.shape[0]
 	A = np.zeros(((nPts * 2), 9))
 	idx = 0
@@ -126,26 +127,4 @@ def computeH_ransac(locs1, locs2):
 
 	# print("Inliers percentage: ",inliers/N)
 	return bestH
-
-
-def compositeH(H2to1, template, img):
-	
-	#Create a composite image after warping the template image on top
-	#of the image using the homography
-
-	#Note that the homography we compute is from the image to the template;
-	#x_template = H2to1*x_photo
-	#For warping the template to the image, we need to invert it.
-	
-
-	#Create mask of same size as template
-
-	#Warp mask by appropriate homography
-
-	#Warp template by appropriate homography
-
-	#Use mask to combine the warped template and the image
-	
-	return composite_img
-
 
