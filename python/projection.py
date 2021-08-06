294d53c1a146606ac514925945237e736c473f48
Ajay Narsimha M <ajaymopidevi@gmail.com>
2021-08-07 02:09:22 +0530

    Correct Warping function

--- python/projection.py
+++ python/projection.py
@@ -1,29 +1,34 @@
 import numpy as np
 
-def Warping(H2to1, template, img):
+def Warping(H, template, img):
 	imgShape = img.shape
 	templateShape = template.shape
 
-	composite_img = img
-	for i in range(imgShape[0]):
-		for j in range(imgShape[1]):
+	for i in range(templateShape[0]):
+		for j in range(templateShape[1]):
 			idx1 = np.array([i, j, 1])
-			idx2 = np.matmul(H2to1, idx1)
+			idx2 = np.matmul(H, idx1)
 			idx2 = idx2 / idx2[-1]
-			if idx2[0] >= 0 and idx2[1] >= 0 and idx2[0] < templateShape[0] and idx2[1] < templateShape[1]:
-				# Intially floor , but later use bilinear interpolation
-				index = [np.int(idx2[0]), np.int(idx2[1])]
-				composite_img[i, j, :] = template[index[0], index[1], :]
-
+			if idx2[0] < 0:
+				idx2[0] =0
+			elif idx2[0] >= imgShape[0]:
+				idx2[0] = imgShape[0]
+			if idx2[1] < 0:
+				idx2[1] = 0
+			elif idx2[1] >= imgShape[1]:
+				idx2[1] = imgShape[1]
+			# Intially floor , but later use bilinear interpolation
+			index = [np.int(idx2[0]), np.int(idx2[1])]
+			composite_img[index[0], index[1], :] = template[i,j,:]
 
 	return composite_img
 
 
-def invWarping(H2to1, template, img):
+def invWarping(H, template, img):
 	imgShape = img.shape
 	templateShape = template.shape
 
-	invH = np.linalg.inv(H2to1)
+	invH = np.linalg.inv(H)
 
 	composite_img = img
 	for i in range(imgShape[0]):
